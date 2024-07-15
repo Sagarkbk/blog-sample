71055c2a8b5deeb50ebbb7513f9bd769e274fa6f
Bavan Krishna Sagar Katnam <114146367+Sagarkbk@users.noreply.github.com>
2024-07-15 12:29:16 +0530

    Update like.ts

--- src/routes/like.ts
+++ src/routes/like.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { blogRouter } from "./blog";
 
 export const likeRouter = new Hono<{
   Bindings: {
